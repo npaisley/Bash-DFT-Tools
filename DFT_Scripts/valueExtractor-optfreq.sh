4c4d62356475ce8e04d32a19d07ee79b792c7086
npaisley <47052925+npaisley@users.noreply.github.com>
2020-09-26 15:25:58 -0700

    Update valueExtractor-optfreq.sh

--- DFT_Scripts/valueExtractor-optfreq.sh
+++ DFT_Scripts/valueExtractor-optfreq.sh
@@ -28,6 +28,9 @@ LUMO=$( grep -A 1 -Ei '^[[:space:]]{1,}Alpha[[:space:]]{1,}occ.' "${1}" | tail -
 #LUMO=($( grep -A 1 -Ei '^[[:space:]]{1,}alpha[[:space:]]{1,}occ' "${1}" | tail -n 1 ))
 LUMO_EV=$( echo "scale=10 ; ${HA_TO_EV} * ${LUMO}" | bc )
 
+#calculate the HOMO LUMO gap
+EGAP=$( echo "scale=10 ; ${LUMO_EV} - ${HOMO_EV}" | bc )
+
 #get the electric dipole if it is present
 if grep -qi 'electric dipole' "${1}"  ; then
 	DIPOLE=($( grep -A 3 -Ei 'electric dipole' "${1}" | tail -n 1 ))
@@ -47,17 +50,21 @@ else
 	METHOD=$( echo "${TOTAL_ENERGY_STRING}" | grep -Eo '^[^[:space:]]{1,}')
 fi
 
-#get singlet and triplet excited state information
+#get singlet and triplet excited state information or state that they haven't been calculated
 if grep -qi 'excitation energies and oscillator strengths:' "${1}"; then
 	SINGLET=($( grep -m 1 -Ei "excited state.{1,}singlet" "${1}" ))
 	TRIPLET=($( grep -m 1 -Ei "excited state.{1,}triplet" "${1}" ))
+	DELTAEST=$( echo "scale=10 ; ${SINGLET[4]} - ${TRIPLET[4]}" | bc )
 else
-	SINGLET="Not calculated"
-	TRIPLET="Not calculated"
+	SINGLET[4]="Not calculated"
+	SINGLET[8]="Not calculated"
+	TRIPLET[4]="Not calculated"
+	TRIPLET[8]="Not calculated"
+	DELTAEST="Not calculated"
 fi	
 
 #output results
 echo "File,Method,HOMO (au),HOMO (eV),LUMO (au),LUMO (eV),Egap (eV),Dipole (Debye),Energy (au),S0 -> S1 (eV),f,S0 -> T1 (eV),f,deltaEst"
-echo "$( basename "${1}" ),${METHOD},${HOMO},${HOMO_EV},${LUMO},${LUMO_EV},$( echo "scale=10 ; ${LUMO_EV} - ${HOMO_EV}" | bc ),${DIPOLE},${SINGLET[4]},${SINGLET[8]},${TRIPLET[4]},${TRIPLET[8]},$( echo "scale=10 ; ${SINGLET[4]} - ${TRIPLET[4]}" | bc )"
+echo "$( basename "${1}" ),${METHOD},${HOMO},${HOMO_EV},${LUMO},${LUMO_EV},${EGAP},${DIPOLE},${SINGLET[4]},${SINGLET[8]},${TRIPLET[4]},${TRIPLET[8]},${DELTAEST}"
 
 exit 0
