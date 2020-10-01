7572b0278274de09199a9051939ce83fd61a5afd
npaisley <47052925+npaisley@users.noreply.github.com>
2020-10-01 08:29:04 -0700

    Update multiComWriter.sh

--- DFT_Scripts/multiComWriter.sh
+++ DFT_Scripts/multiComWriter.sh
@@ -3,56 +3,65 @@
 #requires one argument
 #can be run in for loops to do many files
 #xyz coordinates and basis set are in input file
-## add usage function and print when no file name given
 
 ### Parameters ###
-## organize this section better
-## add example parameters section
-ROUTE="pseudo=read scf=xqc"
-BASIS_SET=("6-31G(d)" "gen")
-DFT_CALC=("SP" "TDA=(50-50,nstates=10)" "TD=(50-50,nstates=10)") #calculations to run (ex. TD, TDA, opt, freq, opt freq, sp, etc.)
+DFT_CALC=("SP" "opt freq") #calculations to run (ex. TD, TDA, opt, freq, opt freq, sp, etc.)
 AB_CALC=("SP")
 #AB_CALC=(${DFT_CALC[@]}) #uncomment this line and comment the one above to make AB and DFT calculations equivalent
+AB_METHOD=("HF" "MP2") #ab initio methods
+DFT_METHOD=("B3LYP" "LC-WHPBE" "CAM-B3LYP") #DFT methods (functionals)
 DFT_SPIN_TREATMENT=("R" "RO" "U") #list of spin treatments (ex. restricted (R), restricted open (RO), and unrestricted (U))
 AB_SPIN_TREATMENT=("R" "RO" "U")
 #AB_SPIN_TREATMENT=(${DFT_SPIN_TREATMENTS[@]}) #uncomment this line and comment the one above to make AB and DFT spin treatments equivalent
-AB_METHODS=("HF" "MP2") #ab initio methods
-DFT_METHODS=("B3LYP" "LC-WHPBE" "CAM-B3LYP") #DFT methods (functionals)
+BASIS_SET=("6-31G(d)" "gen")
+ROUTE_PARAMETERS=("pseudo=read scf=xqc" "geom=check guess=read")
 DFT_CHARGE_MULTIPLICITY=("0 1" "0 3") #list charge and multiplicities here (ex. 0 1, 0 3, etc.)
 AB_CHARGE_MULTIPLICITY=("0 1" "0 3") #list charge and multiplicities here (ex. 0 1, 0 3, etc.)
 #AB_CHARGE_MULTIPLICITY=(${DFT_CHARGE_MULTIPLICITY[@]}) #uncomment this line and comment the one above to make AB and DFT charge and multiplicities equivalent
+MEMORY="62GB" #total amount of memory needed and units.
+CPUS="16" #number of cpus. 8 or 16 works well.
+#example prameters
+## fill out
 ######
 
 com_writer () {
 # $1 is the xyz file
-# $2 is the method name
-# $3 is the route for that method
-# $4 is the charge and multiplicity 
+# $2 is the method (theory) name
+# $3 is the memory
+# $4 is the cpus
+# $5 is the route for that method
+# $6 is the charge and multiplicity 
 
-cat << EOF > ${1}-${2}.com
-%rwf=${1}-${2}.rwf
+cat << EOF > "${1}"-"${2}".com
+%rwf="${1}"-"${2}".rwf
 %nosave
-%chk=${1}-${2}.chk
-%mem=62GB
-%nprocshared=16
-#${3}
+%chk="${1}"-"${2}".chk
+%mem="${3}"
+%nprocshared="${4}"
+#"${5}"
 
-${1} (cm=${4}) with ${3}
+"${1}" (cm="${6}") with "${5}"
 
-${4}
+"${6}"
 $( cat ${FILE_NAME_FULL} )
 
 
 EOF
 }
 
+usage (){
+	echo "run using ./multiComWriter.sh <structure file.xyz>"
+	echo
+}
+
 #make sure file name is provided. if it isnt then exit with an error message
 if [[ -z ${1} ]] ; then
 	echo "an xyz file must be specified"
+	usage
 	exit 1
 fi
 
-#assign full xyz file name and name without the extension
+#assign xyz file name and name without the extension
 FILE_NAME=${1%.*} #remove file extension
 FILE_NAME_FULL=${1}
 
@@ -60,13 +69,29 @@ FILE_NAME_FULL=${1}
 ##have separate potrions for DFT and AB methods
 #make route and write com file
 
+#write DFT com files
+#dft method
+#spin
+
+
+
+
+for CALC in ${DFT_CALC[@]} ; do
+	for METHOD in ${DFT_METHOD[@]} ; do
+		for SPIN in ${DFT_SPIN_TREATMENT[@]} ; do
+			for BASIS in ${BASIS_SET[@]} ; do
+				for ROU in ${ROUTE_PARAMETERS[@]} ; do
+					for CM in ${DFT_CHARGE_MULTIPLICITY[@]} ; do
+						FULL_ROUTE=" "
+						METHOD=""
+			
+
 #assign charge and multiplicity and file name suffix according to method
 for T in ${CALC_TYPE[@]} ; do
 	if [[ ${T} == "SP" ]] ; then #do for all methods
 		for S in ${SPIN_TREATMENT[@]} ; do
 			for M in ${AB_METHODS[@]} ${DFT_METHODS[@]} ; do
 				ROUTE=" ${T} ${S}${M}/gen pseudo=read scf=xqc"
-				#echo ${ROUTE} #for debugging
 				if [[ ${S} == "R" ]] ; then
 					CHARGE_MULTIPLICITY="0 1"
 				else
