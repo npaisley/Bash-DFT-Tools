1baf45986c4037af5745cf06f3df1959f0c439c2
npaisley <47052925+npaisley@users.noreply.github.com>
2021-04-11 02:10:05 -0700

    Update qstat.sh

--- DFT_Scripts/qstat.sh
+++ DFT_Scripts/qstat.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-#cute little script to give you quick and simple information about your slurm queue
+#Nathan R. Paisley, April 2021
+#cute little (well its a bit fat now) script to give you quick and simple information about your slurm queue
 #without any arguments a short summary will display. If the -l argument is used a longer summary will display
-#the -h or any other -<alpha> will display a help message
-#now formatted so that it looks pretty!
+#-h or any other -<alpha> will display a help message
+#now formatted so that it looks pretty! Wow!
+#And with dynamic line lengths! What!?
+### NOTE ###
 #most useful to alias in your .bashrc file
 #for example: in your home directory open your .bashrc file with a text editor of your choosing add the line 
 # alias qs="~/Bash-DFT-Tools/DFT_Scripts/qstat.sh"
@@ -32,10 +35,10 @@ summary_long () {
 for JOBID in $( sq -h | grep -oE '^[[:space:]]{0,}[0-9]{1,}' | xargs ) ; do 
     # if the jobid is found in the scratch file system then print the file that is running
     if $( ls -R ~/scratch/ | grep -q "${JOBID}" ) ; then
-        echo "${JOBID} running: $( ls -R ~/scratch/ | grep "${JOBID}" )"
+        echo "${JOBID} running: $( tput bold )$( ls -R ~/scratch/ | grep "${JOBID}" )$( tput sgr0 )"
     # if the jobid is not found in the scratch folder then search the sg16 log for it
     elif $( grep -a -q "${JOBID}" ~/sg16log/sg16submit.log ) ; then
-        echo "${JOBID} queued: $( grep -a -B 2 "${JOBID}" ~/sg16log/sg16submit.log | head -n 1 | grep -oE '[^/]{1,}$')"
+        echo "${JOBID} queued: $( tput bold )$( grep -a -B 2 "${JOBID}" ~/sg16log/sg16submit.log | head -n 1 | grep -oE '[^/]{1,}$')$( tput sgr0 )"
     # if the jobid cannot be found anywhere inform the user
     else
         echo "${JOBID} not found"
@@ -45,10 +48,10 @@ echo "${LINE}"
 }
 
 usage () {
-LINE="$( echo "if no argument is provided a short summary is printed" | tr [:print:] - )"
+LINE="$( echo "-l : Prints additional information on the queued and running calculations" | tr [:print:] - )"
 echo "${LINE}"
-echo 'usage: ./sqtat.sh [-l] [-h]'
-echo '-l : give longer printout'
+echo "$( tput bold )usage: ./sqtat.sh [-l] [-h]$( tput sgr0 )"
+echo '-l : Prints additional information on the queued and running calculations'
 echo '-h : prints this message'
 echo 'if no argument is provided a short summary is printed'
 echo "${LINE}"
@@ -59,7 +62,7 @@ QUEUE=$(sq)
 while getopts ":l" OPTION ; do
   case ${OPTION} in
     l ) # display long summary
-        # set dynamic line length
+        # set dynamic line length via an overcomplicated method
         LINE=0
         for JOBID in $( sq -h | grep -oE '^[[:space:]]{0,}[0-9]{1,}' | xargs ) ; do 
             if $( ls -R ~/scratch/ | grep -q "${JOBID}" ) ; then
