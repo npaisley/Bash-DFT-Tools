ac6569a40ce443c8fff94c091b429a29fe810138
npaisley <47052925+npaisley@users.noreply.github.com>
2021-04-11 01:48:38 -0700

    Update qstat.sh

--- DFT_Scripts/qstat.sh
+++ DFT_Scripts/qstat.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #cute little script to give you quick and simple information about your slurm queue
+#without any arguments a short summary will display. If the -l argument is used a longer summary will display
+#the -h or any other -<alpha> will display a help message
 #now formatted so that it looks pretty!
 #most useful to alias in your .bashrc file
 #for example: in your home directory open your .bashrc file with a text editor of your choosing add the line 
 # alias qs="~/Bash-DFT-Tools/DFT_Scripts/qstat.sh"
 #to the alias section of your .bashrc file and save
-#make sure qstat.sh can be executed or this won't work
-#you can then use the command qs anytime to run this script
+#you can then use the command qs or qs -l anytime to run this script
 
 summary_short () {
 #total queue
@@ -43,21 +44,40 @@ done
 echo "${LINE}"
 }
 
+usage () {
+LINE="$( echo "if no argument is provided a short summary is printed" | tr [:print:] - )"
+echo "${LINE}"
+echo 'usage: ./sqtat.sh [-l] [-h]'
+echo '-l : give longer printout'
+echo '-h : prints this message'
+echo 'if no argument is provided a short summary is printed'
+echo "${LINE}"
+}
+
 QUEUE=$(sq)
+#parse any given arguments
 while getopts ":l" OPTION ; do
   case ${OPTION} in
     l ) # display long summary
         # set dynamic line length
-        LINE="$( echo "Scratch files to be deleted: $( wc -l < /home/scratch_to_delete/${USER} )" | tr "[:print:]" "-" )"
+        LINE=0
+        for JOBID in $( sq -h | grep -oE '^[[:space:]]{0,}[0-9]{1,}' | xargs ) ; do 
+            if $( ls -R ~/scratch/ | grep -q "${JOBID}" ) ; then
+                LINE_ST="${JOBID} running: $( ls -R ~/scratch/ | grep "${JOBID}" )"
+            elif $( grep -a -q "${JOBID}" ~/sg16log/sg16submit.log ) ; then
+                LINE_ST="${JOBID} queued: $( grep -a -B 2 "${JOBID}" ~/sg16log/sg16submit.log | head -n 1 | grep -oE '[^/]{1,}$')"
+            fi
+            if [[ "$( echo "${LINE_ST}" | wc -L )" -gt "${LINE}" ]] ; then
+                LINE="$( echo "${LINE_ST}" | wc -L )"
+            fi
+        done
+        LINE="$( printf '%0.s-' $(seq 1 ${LINE}) )"
         summary_short
         summary_long
         exit 0
         ;;
     ? ) # display help message
-        echo './sqtat.sh [-l] [-h]'
-        echo '-l : give longer printout'
-        echo '-h : prints this message'
-        echo 'if no argument is provided a short summary is printed'
+        usage
         exit 0
         ;;
   esac
@@ -69,4 +89,3 @@ LINE="$( echo "Scratch files to be deleted: $( wc -l < /home/scratch_to_delete/$
 summary_short
 
 exit 0
-
