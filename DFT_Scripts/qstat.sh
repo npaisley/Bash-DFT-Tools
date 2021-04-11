c21b0b88721a14b3b989a58b1276706cedba31fc
npaisley <47052925+npaisley@users.noreply.github.com>
2021-04-11 01:01:13 -0700

    Update qstat.sh

--- DFT_Scripts/qstat.sh
+++ DFT_Scripts/qstat.sh
@@ -8,11 +8,13 @@
 #make sure qstat.sh can be executed or this won't work
 #you can then use the command qs anytime to run this script
 
-QUEUE=$(sq)
-#total
+summary_short () {
+# set dynamic line length
+LINE="$( echo "Scratch files to be deleted: $( wc -l < /home/scratch_to_delete/${USER} )" | tr "[:print:]" "-" )"
+#total queue
 TOTAL=$( echo "${QUEUE}" | wc -l )
 # use echo "words" | tr "[:print:]" "-" for a dynamic line
-echo "------------------------------"
+echo "${LINE}"
 echo "$( tput bold )Total queue:$(tput sgr0)" $(( TOTAL - 1 ))
 #running
 echo " $( tput bold )Running:$(tput sgr0)   " $( echo "${QUEUE}" | grep -c '(None)' )
@@ -21,10 +23,47 @@ echo " $( tput bold )Priority:$(tput sgr0)  " $( echo "${QUEUE}" | grep -c '(Pri
 #priority
 echo " $( tput bold )Resources:$(tput sgr0) " $( echo "${QUEUE}" | grep -c '(Resources)' )
 #resources
-echo "------------------------------"
+echo "${LINE}"
 echo "$( tput bold )Scratch files to be deleted:$(tput sgr0)" $( wc -l < /home/scratch_to_delete/${USER} )
-echo "------------------------------"
+echo "${LINE}"
+}
 
-# use below to display files that correspond to the running calculations
-# for I in $( sq -h | grep -oE '^[[:space:]]{0,}[0-9]{1,}' | xargs ) ; do ls -R ~/scratch/ | grep "${I}" ; done
+summary_long () {
+# print the jobids of the running jobs and their corresponding files
+for JOBID in $( sq -h | grep -oE '^[[:space:]]{0,}[0-9]{1,}' | xargs ) ; do 
+    # if the jobid is found in the scratch file system then print the file that is running
+    if $( ls -R ~/scratch/ | grep -q "${JOBID}" ) ; then
+        echo "${JOBID} running: $( ls -R ~/scratch/ | grep "${JOBID}" )"
+    # if the jobid is not found in the scratch folder then search the sg16 log for it
+    elif $( grep -a -q "${JOBID}" ~/sg16log/sg16submit.log ) ; then
+        echo "${JOBID} queued: $( grep -a -B 2 "${I}" ~/sg16log/sg16submit.log | head -n 1 | grep -oE '[^/]{1,}$')"
+    # if the jobid cannot be found anywhere inform the user
+    else
+        echo "${JOBID} not found"
+    fi
+done
+}
+
+QUEUE=$(sq)
+while getopts ":l" OPTION ; do
+  case ${OPTION} in
+    l ) # display long summary
+        summary_short
+        summary_long
+        exit 0
+        ;;
+    h ) # display help message
+        echo "./sqtat.sh [-l] [-h]"
+        echo "-l : give longer printout
+        echo "-h : prints this message"
+        echo "if no argument is provided a short summary is printed"
+        exit 0
+        ;;
+    \? ) # display short summary
+        summary_short
+        exit 0
+        ;;
+  esac
+done
+
 exit 0
